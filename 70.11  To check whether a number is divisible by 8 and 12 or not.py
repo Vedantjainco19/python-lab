47bf1fb4caa39156fdec6351a43332f2730e5b2b
Vedantjainco19 <82804192+Vedantjainco19@users.noreply.github.com>
2021-11-17 22:09:18 +0530

    Create 70.11  To check whether a number is divisible by 8 and 12 or not.py

---
+++ 70.11  To check whether a number is divisible by 8 and 12 or not.py
@@ -0,0 +1,8 @@
+#To check whether a number is divisible by 8 and 12 or not.
+a=int(input("enter any number"))
+if a%8==0 :
+  print("the number is divisible by 8")
+elif a%12==0  :
+  print("the number is divisible by 12")
+else :
+  print("the number is not divisible by 8 or 12")
